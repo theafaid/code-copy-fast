3effafe3d786884d620a2d5a3accf4460be8a1db
A||F <abdurrhmanfaid@gmail.com>
2020-12-31 19:42:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# A placed element for copying code fast through mouse click
+
+Rather than select the whole code content using your mouse then copy you can Just click on copy button directly.
+
+![Alt text](screenshot.png?raw=true "Title")
+
+
+## How to install?
+
+1. Clone this repository locally or [download repository as ZIP](https://github.com/abdurrhmanFaid/code-copy-fast/archive/master.zip) and unzip it locally.
+2. In Chrome, at the top right, click `More`, then `More tools`, and then `Extensions`.
+3. At the top left, click `Upload unpacked extension` and select directory where you unzipped it.
+4. You are able to copy code content fast.
---
+++ main.js
@@ -0,0 +1,45 @@
+const notificationTimeout  = 2000
+
+const copierStyle = `
+            cursor: pointer;
+            position: relative;
+            background: #000;
+            float: right;
+            border-radius: 5px;
+            color: #fff;
+            font-weight:  bold;
+            padding: 4px;
+            font-size: 10px;
+            margin-right: 5px`;
+
+const codeElements = ['code', 'samp'];
+
+let startIdx = 0;
+
+codeElements.forEach((codeElement) => {
+    document.querySelectorAll(codeElement).forEach(ele => {
+        if (! validCodeElement(ele)) return;
+        ele.setAttribute('data-code-idx', startIdx);
+        ele.innerHTML += '<br>';
+        const copier = document.createElement('span');
+        copier.setAttribute('data-code-idx', startIdx);
+        copier.setAttribute('style', copierStyle);
+        copier.textContent = "Copy";
+        copier.addEventListener('click', copierClicked);
+        ele.parentElement.append(copier);
+        startIdx++;
+    })
+})
+
+function validCodeElement(ele) {
+    return ele.parentElement.tagName === 'PRE';
+}
+
+function copierClicked(e) {
+    const idx = e.target.getAttribute('data-code-idx');
+    if (! idx) return;
+    const value = document.querySelector('[data-code-idx="' + idx + '"]');
+    navigator.clipboard.writeText(value.textContent);
+    e.target.innerHTML = 'Copied <span style="color: #3d92ee">&#10004;</span>';
+    setTimeout(() => e.target.textContent = "Copy", notificationTimeout);
+}
